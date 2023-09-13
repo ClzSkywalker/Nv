13627b180c0abdedfe7dcce4497054a42e742433
ClzSkywalker <44250611+ClzSkywalker@users.noreply.github.com>
2023-09-13 17:16:56 +0800

    Update init.lua

--- init.lua
+++ init.lua
@@ -6,7 +6,7 @@ require("config.lazy")({
     -- cond = false,
   },
   nv = {
-    colorscheme = "onelight", -- colorscheme setting for either onedark.nvim or github-theme
+    colorscheme = "gruvbox", -- colorscheme setting for either onedark.nvim or github-theme
     codeium_support = false, -- enable codeium extension
     copilot_support = false, -- enable copilot extension
     coverage_support = true, -- enable coverage extension
@@ -15,10 +15,10 @@ require("config.lazy")({
       clangd = false, -- enable clangd and cmake extension
       docker = true, -- enable docker extension
       elixir = false, -- enable elixir extension
-      go = false, -- enable go extension
+      go = true, -- enable go extension
       java = false, -- enable java extension
-      nodejs = true, -- enable nodejs (typescript, css, html, json) extension
-      python = true, -- enable python extension
+      nodejs = false, -- enable nodejs (typescript, css, html, json) extension
+      python = false, -- enable python extension
       ruby = false, -- enable ruby extension
       rust = true, -- enable rust extension
       terraform = false, -- enable terraform extension
